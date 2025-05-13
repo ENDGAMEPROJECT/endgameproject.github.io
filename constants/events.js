8071046aecb044c8711269e9aa9babe59d349077
Enrique <enrique.barra@upm.es>
2025-05-13 12:59:45 +0200

    events order

--- constants/events.js
+++ constants/events.js
@@ -1,63 +1,4 @@
 export const events = [
-  {
-    eventname: "WorkshopSpain",
-    category: "Workshop", // + online?
-    country: "Spain",
-    type: "Event",
-    date: "05/07/25",
-    hour: "17:00 UTC+1",
-    title_en: "Narratives under the Magnifying Glass: Designing Educational Tools against Disinformation",
-    title_es: "Narrativas bajo la Lupa: Diseñando Herramientas Educativas contra la Desinformación",
-    description_en: `If you work in journalism or teaching, are a student or work with young people, we need you! 
-    
-On Wednesday, May 7 at 5:00 pm we invite you to participate in a one-hour online session in which we will talk, share ideas and listen to your experience to analyze the digital habits and misinforming narratives faced by Spanish youth.  
-
-In this session, experts will contribute their opinions and experiences to help shape various educational materials to help youth recognize disinformation and strengthen their critical thinking. We want these resources to be truly useful, accessible and connected to the real needs of society. To achieve this, we need people like you. Having you is a huge help to all of us who are designing this project. `,
-    description_es: `
-Si te dedicas al periodismo o la docencia, eres estudiante o trabajas con jóvenes, ¡te necesitamos! 
-    
-El miércoles 7 de mayo a las 17:00 h (hora Madrid) te invitamos a participar en una sesión online de una hora en la que conversaremos, compartiremos ideas y escucharemos tu experiencia para analizar los hábitos digitales y las narrativas desinformadoras a las que se enfrenta la juventud española.  
-
-En esta sesión, personas expertas aportarán su opinión y vivencias para ayudar a dar forma a diversos materiales educativos que ayuden a a juventud a reconocer la desinformación y a fortalecer su pensamiento crítico. Queremos que estos recursos sean verdaderamente útiles, accesibles y conectados con las necesidades reales de la sociedad. Para lograrlo, necesitamos contar con personas como tú. Contar contigo es una enorme ayuda para todas las personas que estamos diseñando este proyecto. `,
-// short description -> eventCard
-    externalLinkJoin: "https://forms.gle/6HugpAZw7oSqDTc56",
-    keywords: ["Workshop", "Media literacy", "misinformation"]
-  },
-     {
-    eventname: "WorkshopSerbia",
-    category: "Workshop",
-    country: "Serbia",
-    type: "Event",
-    date: "05/14/25",
-    hour: "18:00 UTC+1",
-    title_en:  "Online workshop: Identifying disinformation",
-    title_sr: "Virtuelna radionica: Identifikacija dezinformacija",
-    description_en:  ` If you work in journalism or teaching, are a student or work with young people, we need you!
-   
-On Wednesday, May 14 at 6:00 pm we invite you to participate in a one-hour online session in which we will talk, share ideas and listen to your experience to analyze the digital habits and misinforming narratives faced by Serbian youth.  
- 
-In this session, experts will contribute their opinions and experiences to help shape various educational materials to help youth recognize disinformation and strengthen their critical thinking. We want these resources to be truly useful, accessible and connected to the real needs of society. To achieve this, we need people like you. Having you is a huge help to all of us who are designing this project.  `,
-    description_sr: `Ako radite u novinarstvu ili nastavi, student ste ili radite sa mladima, potrebni ste nam!
-
-U sredu, 14. maja u 18:00 časova pozivamo vas da učestvujete u jednočasovnoj onlajn sesiji u kojoj ćemo razgovarati, deliti ideje i slušati vaša iskustva kako bismo analizirali digitalne navike i dezinformacije sa kojima se suočavaju mladi u Srbiji.
-U ovoj sesiji, stručnjaci će doprineti svojim razmišljanjima i iskustvom kako bi pomogli u oblikovanju različitih obrazovnih materijala koji će pomoći mladima da prepoznaju dezinformacije i ojačaju svoje kritičko rmišljenje. Želimo da ovi resursi budu zaista korisni, pristupačni i povezani sa stvarnim potrebama društva. Da bismo to postigli, potrebni su nam ljudi poput vas. Vaše prisustvo je ogromna pomoć svima nama koji radimo na ovom projektu. `,
-    externalLinkJoin: "https://forms.gle/aL2x5URUz1sEbJLaA ",
-    keywords: ["Workshop", "Media literacy", "misinformation"],
-  },
-  {
-    eventname: "WorkshopFinland",
-    category: "Workshop",
-    country: "Finland",
-    type: "Event",
-    date: "05/16/25",
-    hour: "12:00 UTC+3",
-    title_en:  "Misinformation narratives in Finland",
-    description_en:  `We’re inviting participants to take part in a media literacy workshop as part of the EU-funded ENDGAME project. If you are between 18 and 30 years old and are tired of being exposed to misinformation (e.g., related to migrants, climate change, or cryptocurrency), we want to hear your experience! 
-
-    In this interactive session, you'll help identify the most pressing misinformation narratives and media literacy challenges facing young people today. The results will be used to design educational escape rooms that train critical thinking and digital resilience.`,
-    externalLinkJoin: "https://docs.google.com/forms/d/e/1FAIpQLScyhbCM1wKiRlkt-z8dfbtDKazVuA3T2Xh_1fJ3BZJMSWcLtg/viewform",
-    keywords: ["Workshop", "Media literacy", "misinformation"],
-  },
   { 
     eventname: "ParticipatoryDesignWorkshop",
     category: "Workshop",
@@ -86,5 +27,63 @@ Sarjassa eri vaiheisiin ajoittuvia yhteissuunnittelutyöpajoja määrittelemme p
     externalLinkJoin: "https://uef.zoom.us/meeting/register/d-Ok41vZS_W_fMGyBERFAw#/registration",
     keywords: ["Workshop", "Media literacy", "misinformation", "Escape room", "Participatory design"],
   },
-  
+  {
+    eventname: "WorkshopFinland",
+    category: "Workshop",
+    country: "Finland",
+    type: "Event",
+    date: "05/16/25",
+    hour: "12:00 UTC+3",
+    title_en:  "Misinformation narratives in Finland",
+    description_en:  `We’re inviting participants to take part in a media literacy workshop as part of the EU-funded ENDGAME project. If you are between 18 and 30 years old and are tired of being exposed to misinformation (e.g., related to migrants, climate change, or cryptocurrency), we want to hear your experience! 
+
+    In this interactive session, you'll help identify the most pressing misinformation narratives and media literacy challenges facing young people today. The results will be used to design educational escape rooms that train critical thinking and digital resilience.`,
+    externalLinkJoin: "https://docs.google.com/forms/d/e/1FAIpQLScyhbCM1wKiRlkt-z8dfbtDKazVuA3T2Xh_1fJ3BZJMSWcLtg/viewform",
+    keywords: ["Workshop", "Media literacy", "misinformation"],
+  },
+   {
+    eventname: "WorkshopSerbia",
+    category: "Workshop",
+    country: "Serbia",
+    type: "Event",
+    date: "05/14/25",
+    hour: "18:00 UTC+1",
+    title_en:  "Online workshop: Identifying disinformation",
+    title_sr: "Virtuelna radionica: Identifikacija dezinformacija",
+    description_en:  ` If you work in journalism or teaching, are a student or work with young people, we need you!
+   
+On Wednesday, May 14 at 6:00 pm we invite you to participate in a one-hour online session in which we will talk, share ideas and listen to your experience to analyze the digital habits and misinforming narratives faced by Serbian youth.  
+ 
+In this session, experts will contribute their opinions and experiences to help shape various educational materials to help youth recognize disinformation and strengthen their critical thinking. We want these resources to be truly useful, accessible and connected to the real needs of society. To achieve this, we need people like you. Having you is a huge help to all of us who are designing this project.  `,
+    description_sr: `Ako radite u novinarstvu ili nastavi, student ste ili radite sa mladima, potrebni ste nam!
+
+U sredu, 14. maja u 18:00 časova pozivamo vas da učestvujete u jednočasovnoj onlajn sesiji u kojoj ćemo razgovarati, deliti ideje i slušati vaša iskustva kako bismo analizirali digitalne navike i dezinformacije sa kojima se suočavaju mladi u Srbiji.
+U ovoj sesiji, stručnjaci će doprineti svojim razmišljanjima i iskustvom kako bi pomogli u oblikovanju različitih obrazovnih materijala koji će pomoći mladima da prepoznaju dezinformacije i ojačaju svoje kritičko rmišljenje. Želimo da ovi resursi budu zaista korisni, pristupačni i povezani sa stvarnim potrebama društva. Da bismo to postigli, potrebni su nam ljudi poput vas. Vaše prisustvo je ogromna pomoć svima nama koji radimo na ovom projektu. `,
+    externalLinkJoin: "https://forms.gle/aL2x5URUz1sEbJLaA ",
+    keywords: ["Workshop", "Media literacy", "misinformation"],
+  },
+  {
+    eventname: "WorkshopSpain",
+    category: "Workshop", // + online?
+    country: "Spain",
+    type: "Event",
+    date: "05/07/25",
+    hour: "17:00 UTC+1",
+    title_en: "Narratives under the Magnifying Glass: Designing Educational Tools against Disinformation",
+    title_es: "Narrativas bajo la Lupa: Diseñando Herramientas Educativas contra la Desinformación",
+    description_en: `If you work in journalism or teaching, are a student or work with young people, we need you! 
+    
+On Wednesday, May 7 at 5:00 pm we invite you to participate in a one-hour online session in which we will talk, share ideas and listen to your experience to analyze the digital habits and misinforming narratives faced by Spanish youth.  
+
+In this session, experts will contribute their opinions and experiences to help shape various educational materials to help youth recognize disinformation and strengthen their critical thinking. We want these resources to be truly useful, accessible and connected to the real needs of society. To achieve this, we need people like you. Having you is a huge help to all of us who are designing this project. `,
+    description_es: `
+Si te dedicas al periodismo o la docencia, eres estudiante o trabajas con jóvenes, ¡te necesitamos! 
+    
+El miércoles 7 de mayo a las 17:00 h (hora Madrid) te invitamos a participar en una sesión online de una hora en la que conversaremos, compartiremos ideas y escucharemos tu experiencia para analizar los hábitos digitales y las narrativas desinformadoras a las que se enfrenta la juventud española.  
+
+En esta sesión, personas expertas aportarán su opinión y vivencias para ayudar a dar forma a diversos materiales educativos que ayuden a a juventud a reconocer la desinformación y a fortalecer su pensamiento crítico. Queremos que estos recursos sean verdaderamente útiles, accesibles y conectados con las necesidades reales de la sociedad. Para lograrlo, necesitamos contar con personas como tú. Contar contigo es una enorme ayuda para todas las personas que estamos diseñando este proyecto. `,
+// short description -> eventCard
+    externalLinkJoin: "https://forms.gle/6HugpAZw7oSqDTc56",
+    keywords: ["Workshop", "Media literacy", "misinformation"]
+  }  
 ];
