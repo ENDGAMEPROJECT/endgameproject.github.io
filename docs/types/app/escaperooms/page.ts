e411b65b38a3a1d4846344dcefd719fe33597594
Enrique <enrique.barra@upm.es>
2025-10-28 11:13:04 +0100

    docs folder added

--- docs/types/app/escaperooms/page.ts
+++ docs/types/app/escaperooms/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/leo/Desktop/UPM/endgameproject.github.io/app/escaperooms/page.js
+// File: C:\Users\barra\trabajo\endgame.github.io\app\escaperooms\page.js
 import * as entry from '../../../../app/escaperooms/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- docs/types/app/page.ts
+++ docs/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/leo/Desktop/UPM/endgameproject.github.io/app/page.js
+// File: C:\Users\barra\trabajo\endgame.github.io\app\page.js
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
