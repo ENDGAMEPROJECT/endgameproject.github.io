c0863274a75dadcc26d4ed908e20d462df02c196
Enrique <enrique.barra@upm.es>
2026-04-30 13:01:50 +0200

    teaching materials for production website

--- docs/_next/static/AI2Z8Slrs6aMo1VICUpnh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fescaperooms\u002F[escaperoomname]","\u002Fescaperooms\u002F[escaperoomname]\u002Fplay","\u002Fevents\u002F[eventname]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
